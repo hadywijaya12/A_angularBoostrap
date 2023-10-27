3514794b98d334c36b480045c59aec95224f1473
hadywijaya12 <hadywijaya171103@gmail.com>
2023-10-27 23:03:09 +0700

    a
    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var window:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  nama = "Hady Wijaya";
+  nim = "212102381";
   title = 'angularBootstrap';
+  formModal:any;
+
+  ngOnInit(): void {
+      this.formModal = new window.bootstrap.Modal(
+        document.getElementById("exampleModal")
+      )
+  }
+
+  openModal(){
+    this.formModal.show();
+  }
+
+  doSomething(){
+    this.formModal.hide();
+  }
 }
